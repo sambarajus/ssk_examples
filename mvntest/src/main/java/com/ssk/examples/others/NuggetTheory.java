99bb303ef6029210590a9889d59c4d159da0797e
shravans <shravans@shravans-pc>
2014-12-16 02:13:14 -0800

    Nugget Problem refactored

--- mvntest/src/main/java/com/ssk/examples/others/NuggetTheory.java
+++ mvntest/src/main/java/com/ssk/examples/others/NuggetTheory.java
@@ -9,45 +9,67 @@ package com.ssk.examples.others;
  */
 public class NuggetTheory {
 
-	//define a static array of allowed sizes..
-	static int[] sizes = {6,9,20};
-	static int[] combos = {0,0,0};
+	// define a static array of allowed sizes..
+	static int[] sizes = { 6, 9, 20 };
+	static int[] combos = { 0, 0, 0 };
 
-	// This is based on GreedyAlgorithm http://mathworld.wolfram.com/GreedyAlgorithm.html
-	/** 
+	// This is based on GreedyAlgorithm
+	// http://mathworld.wolfram.com/GreedyAlgorithm.html
+	/**
 	 * @param n
 	 * @return
 	 */
-	
-	public static boolean isSellable(int n){
-		for (int i = 0;i<sizes.length;i++){
-			int c = n/sizes[i];
-			combos[i]=c;
-			if(c*sizes[i]!=0){
-				int diff = n-c*sizes[i];
-				for (int j = 0;j<i;j++){
-					int cj = diff/sizes[j];
-					combos[j]=cj;
+
+	public static boolean isSellable(int n) {
+		for (int i = 0; i < sizes.length; i++) {
+			int c = n / sizes[i];
+			combos[i] = c;
+			if (c * sizes[i] != 0) {
+				int diff = n - c * sizes[i];
+				for (int j = 0; j < i; j++) {
+					int cj = diff / sizes[j];
+					combos[j] = cj;
 				}
 			}
 		}
 		int sum = 0;
 		for (int i = 0; i < combos.length; i++) {
-			sum = sum+sizes[i]*combos[i];
+			sum = sum + sizes[i] * combos[i];
 		}
-		if(sum==n)
+		if (sum == n)
 			return true;
 		return false;
 	}
 
-	public static void main(String[] args){
-		System.out.println(isSellable(15));
-		printCombinations(sizes,combos);
+	public static void main(String[] args) {
+//		System.out.println(isSellable(15));
+//		printCombinations(sizes, combos);
+		System.out.println(sellableNuggets(6));
+		System.out.println(sellableNuggets(16));
+		System.out.println(sellableNuggets(15));
+		System.out.println(sellableNuggets(62));
 	}
-	private static void printCombinations(int[] a,int[] b){
+
+	private static void printCombinations(int[] a, int[] b) {
 		System.out.println();
 		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i]+" * "+b[i]+", ");
+			System.out.print(a[i] + " * " + b[i] + ", ");
 		}
+	}
+
+	public static boolean sellableNuggets(int num) {
+		int minusSix = num - 6;
+		int minusNine = num - 9;
+		int minusTwenty = num - 20;
+		if (minusSix == 0) {
+			return true; // 6
+		} else if (minusNine == 0) {
+			return true; // 9
+		} else if (minusTwenty == 0) {
+			return true;
+		} else if (minusSix < 0) {
+			return false;
+		}
+		return sellableNuggets(minusSix) || sellableNuggets(minusNine) || sellableNuggets(minusTwenty);
 	}
 }
